450899dc727866eefe8137b53c7d35c4382fd462
BigjoeTech01 <ekenzefranklinejoe2020@gmail.com>
2026-07-04 12:01:17 +0100

    task_t2 commit

--- lib/2_application/core/routes.dart
+++ lib/2_application/core/routes.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:todo_app/2_application/app/pages/home/home_page.dart';
+import 'package:todo_app/2_application/app/pages/settings/settings_page.dart';
+import 'package:todo_app/2_application/app/pages/task/tasks_page.dart';
 //import 'package:todo_app/2_application/app/pages/home/home_page.dart';
 import 'package:todo_app/2_application/core/go_router_observer.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>(
   debugLabel: 'root',
 );
-/*final GlobalKey<NavigatorState> _shellNavigatorKey = GlobalKey<NavigatorState>(
+final GlobalKey<NavigatorState> _shellNavigatorKey = GlobalKey<NavigatorState>(
   debugLabel: 'shell',
-);*/
+);
 
 final routes = GoRouter(
   observers: [GoRouterObserver()],
   navigatorKey: _rootNavigatorKey,
-  initialLocation: '/home/task',
+  initialLocation: '/home/dashboard',
   routes: [
-    /*ShellRoute(
+    ShellRoute(
       navigatorKey: _shellNavigatorKey,
       builder: (context, state, child) => child,
       routes: [
@@ -27,62 +30,12 @@ final routes = GoRouter(
           ),
         ),
       ],
-    ),*/
-    GoRoute(
-      path: '/home/task',
-      builder: (context, state) => Container(
-        color: Colors.green,
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                context.push('/home/settings');
-              },
-              child: Text('Go to Settings'),
-            ),
-            SizedBox(height: 10),
-
-            ElevatedButton(
-              onPressed: () {
-                if (context.canPop()) {
-                  context.pop();
-                } else {
-                  context.push('/home/task');
-                }
-              },
-              child: Text('Go Back'),
-            ),
-          ],
-        ),
-      ),
     ),
+    GoRoute(path: '/home/tasks', builder: (context, state) => TasksPage()),
 
     GoRoute(
       path: '/home/settings',
-      builder: (context, state) => Container(
-        color: Colors.tealAccent,
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                context.push('/home/task');
-              },
-              child: Text('Go to task'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                if (context.canPop()) {
-                  context.pop();
-                } else {
-                  context.push('/home/task');
-                }
-              },
-              child: Text('Go Back'),
-            ),
-          ],
-        ),
-      ),
+      builder: (context, state) => const SettingsPage(),
     ),
   ],
 );
